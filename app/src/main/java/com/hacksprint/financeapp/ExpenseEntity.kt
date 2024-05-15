f5afb3856ccd048bc226414ca6bdf76ced9da3f3
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-05-15 23:39:49 +0100

    init commit

---
+++ app/src/main/java/com/hacksprint/financeapp/ExpenseEntity.kt
@@ -0,0 +1,22 @@
+package com.hacksprint.financeapp
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = CategoryEntity::class,
+            parentColumns = ["name"],
+            childColumns = ["category"]
+        )
+    ]
+)
+data class ExpenseEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val amount: Double,
+    val category: String,
+    val description: String
+)
