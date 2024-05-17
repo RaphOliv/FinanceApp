22d7173896e67d07d986ebe8adbb8d91854b551b
Raphael Oliveira <164554818+RaphOliv@users.noreply.github.com>
2024-05-17 21:43:49 +0100

    init commit

--- app/src/main/java/com/hacksprint/financeapp/DataBase/FinanceAppDatabase.kt
+++ app/src/main/java/com/hacksprint/financeapp/DataBase/FinanceAppDatabase.kt
@@ -2,7 +2,6 @@ package com.hacksprint.financeapp.DataBase
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.hacksprint.financeapp.ExpenseEntity
 
 @Database(entities = [CategoryEntity::class, ExpenseEntity::class], version = 1)
 abstract class FinanceAppDatabase : RoomDatabase(){
