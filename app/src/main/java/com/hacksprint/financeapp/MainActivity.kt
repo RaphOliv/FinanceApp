d28a3d3548f3e8fac747fde2e2bfb14639c211f3
BrunoHPBarbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2024-05-19 00:19:00 +0100

    ajustes em algumas classes

--- app/src/main/java/com/hacksprint/financeapp/MainActivity.kt
+++ app/src/main/java/com/hacksprint/financeapp/MainActivity.kt
@@ -3,49 +3,46 @@ package com.hacksprint.financeapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-
+import com.hacksprint.financeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigation: BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        bottomNavigation = findViewById(R.id.bottom_navigation)
-        bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
+
+        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.homeIcon -> {
                     replaceFragment(HomeFragment())
                     true
                 }
-
                 R.id.listaIcon -> {
                     replaceFragment(ListFragment())
                     true
                 }
-
                 R.id.graficIcon -> {
                     replaceFragment(GraficFragment())
                     true
                 }
-
                 R.id.profileIcon -> {
                     replaceFragment(ProfileFragment())
                     true
                 }
-
                 else -> false
-
             }
         }
 
-    if (savedInstanceState == null) {
-        replaceFragment(HomeFragment())
-        bottomNavigation.selectedItemId = R.id.homeIcon
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+            binding.bottomNavigation.selectedItemId = R.id.homeIcon
+        }
     }
-}
-    private fun  replaceFragment(fragment: Fragment){
+
+    private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
     }
 }
