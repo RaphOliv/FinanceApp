60458abb7f2b57d762d878656118e52748203ed7
BrunoHPBarbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2024-05-19 13:22:02 +0100

    ajustes em algumas classes, novamente

--- app/src/main/java/com/hacksprint/financeapp/ListFragment.kt
+++ app/src/main/java/com/hacksprint/financeapp/ListFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ListFragment: Fragment() {
 
     private var rvCategory: RecyclerView? = null
     private var rvExpenses: RecyclerView? = null
-
+    private lateinit var openBottomSheetButton: ImageView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,12 +27,21 @@ class ListFragment: Fragment() {
 
         rvCategory = view.findViewById<RecyclerView>(R.id.rv_categories)
         rvExpenses = view.findViewById<RecyclerView>(R.id.rv_expenses)
+
+openBottomSheetButton.findViewById<ImageView>(R.id.btn_add_categorie)
+        openBottomSheetButton.setOnClickListener {
+            val bottomSheetFragment = DielogList()
+            bottomSheetFragment.show(parentFragmentManager, bottomSheetFragment.tag)
+        }
     }
 
     override fun onResume() {
         super.onResume()
 
+
+
         val categoryListAdapter = CategoryListAdapter()
+
         categoryListAdapter.setOnClickListener { category ->
 
         }
