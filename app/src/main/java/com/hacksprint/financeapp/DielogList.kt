fbc47ec44f259ea78dab54d883b1268ae45520b7
BrunoHPBarbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2024-05-20 07:42:00 +0100

    ajustes em algumas classes, novamente2

---
+++ app/src/main/java/com/hacksprint/financeapp/DielogList.kt
@@ -0,0 +1,20 @@
+package com.hacksprint.financeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class DielogList: BottomSheetDialogFragment()  {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.create_card_list,container,false)
+
+    }
+}
