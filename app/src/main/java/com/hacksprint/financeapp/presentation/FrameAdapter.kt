cf522accfebaf444a14dd533fb0299bc894071a3
BrunoHPBarbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2024-05-21 22:12:26 +0100

    fiz a chamada do dielog na home , ajutei as cores dos dielogs ..

--- app/src/main/java/com/hacksprint/financeapp/presentation/FrameAdapter.kt
+++ app/src/main/java/com/hacksprint/financeapp/presentation/FrameAdapter.kt
@@ -1,8 +1,10 @@
-package com.hacksprint.financeapp
+package com.hacksprint.financeapp.presentation
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.hacksprint.financeapp.FrameLayoutFragment
+import com.hacksprint.financeapp.R
 
 class FrameAdapter (fragmentActivity: FragmentActivity):FragmentStateAdapter(fragmentActivity) {
 
