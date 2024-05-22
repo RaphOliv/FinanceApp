013dcf107525f2a3b38ae51cbcaf34ede158655b
BrunoHPBarbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2024-05-22 22:53:35 +0100

    implementei os icones no dielog , mas nao consigo rodar o app, por causa da base de dados

--- app/src/main/java/com/hacksprint/financeapp/CreateOrUpdateExpenseBottomSheet.kt
+++ app/src/main/java/com/hacksprint/financeapp/CreateOrUpdateExpenseBottomSheet.kt
@@ -1,26 +1,28 @@
 package com.hacksprint.financeapp
 
 import FinanceAppViewModel
-import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.textfield.TextInputEditText
 import com.hacksprint.financeapp.Adapters.ExpenseListAdapter
+import com.hacksprint.financeapp.Adapters.ListIconsAdapter
 import com.hacksprint.financeapp.data.CategoryEntity
 import com.hacksprint.financeapp.data.ExpenseUiData
-import java.text.SimpleDateFormat
-import java.util.Calendar
 
 class CreateOrUpdateExpenseBottomSheet(
-    private val viewModelFinance: FinanceAppViewModel,
+    private val viewModelFinance:FinanceAppViewModel,
     private val adapterFinance: ExpenseListAdapter,
     private val categoryList: List<CategoryEntity>,
     private val expense: ExpenseUiData? = null,
@@ -29,12 +31,6 @@ class CreateOrUpdateExpenseBottomSheet(
     private val onDeleteClicked: (ExpenseUiData) -> Unit
 ) : BottomSheetDialogFragment() {
 
-    private lateinit var edtExpenseName: TextInputEditText
-    private lateinit var edtExpenseAmount: TextInputEditText
-    private lateinit var edtExpenseDate: TextInputEditText
-    private lateinit var btnCreateOrUpdate: Button
-    private lateinit var spinner: Spinner
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -42,79 +38,117 @@ class CreateOrUpdateExpenseBottomSheet(
     ): View? {
         val view = inflater.inflate(R.layout.create_or_update_expense_bottom_sheet, container, false)
 
-        // Inicialize os EditTexts e o Button
-        edtExpenseName = view.findViewById(R.id.et_expense_name)
-        edtExpenseAmount = view.findViewById(R.id.et_expense_amount)
-        edtExpenseDate = view.findViewById(R.id.et_expense_date)
-        btnCreateOrUpdate = view.findViewById(R.id.btn_expense_create)
-        spinner = view.findViewById(R.id.category_spinner)
+        // Inicialize o RecyclerView e o Adapter
+        val recyclerViewIcons = view.findViewById<RecyclerView>(R.id.recyclerListIcons)
+        val icons = listOf(
+            R.drawable.ic_home,
+            R.drawable.baseline_wifi_24,
+            R.drawable.baseline_water_drop_24,
+            R.drawable.baseline_videogame_asset_24,
+            R.drawable.baseline_shopping_cart_24,
+            R.drawable.baseline_local_gas_station_24,
+            R.drawable.baseline_local_airport_24,
+            R.drawable.baseline_electric_bolt_24,
+            R.drawable.baseline_directions_car_24,
+            R.drawable.baseline_credit_card_24,
+            R.drawable.baseline_bar_chart_24
+        )
+        val listIconsAdapter = ListIconsAdapter(icons)
+        recyclerViewIcons.adapter = listIconsAdapter
+        recyclerViewIcons.layoutManager = GridLayoutManager(context, 5)
 
-        // Configuração do Spinner
-        val categoryNames = categoryList.map { it.name }
-        val categoryAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, categoryNames)
-        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = categoryAdapter
+        // chama os edit texts e o botao
 
-        // Preenchendo os EditTexts se a despesa existir
-        expense?.let {
-            edtExpenseName.setText(it.description)
-            edtExpenseAmount.setText(it.amount.toString())
-            edtExpenseDate.setText(it.date)
-            val categoryIndex = categoryNames.indexOf(it.category)
-            spinner.setSelection(categoryIndex)
+        val tvTitle = view.findViewById<TextView>(R.id.tv_expense_title)
+        val edtExpenseName = view.findViewById<TextInputEditText>(R.id.et_expense_name)
+        val edtExpenseAmount = view.findViewById<TextInputEditText>(R.id.et_expense_amount)
+        val edtExpenseDate = view.findViewById<TextInputEditText>(R.id.et_expense_date)
+        val btnCreateOrUpdate = view.findViewById<Button>(R.id.btn_expense_create)
+        val spinner: Spinner = view.findViewById(R.id.category_spinner)
+
+        var expenseCategory: String? = null
+        val categoryListTemp = mutableListOf("Select a category")
+        categoryListTemp.addAll(categoryList.map { it.name })
+
+        ArrayAdapter(
+            requireActivity().baseContext,
+            android.R.layout.simple_spinner_item,
+            categoryListTemp.toList()
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
         }
 
-        // Listener para o EditText de data
-        edtExpenseDate.setOnClickListener {
-            showDatePickerDialog()
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                expenseCategory = categoryListTemp[position]
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                showMessages("Category is required")
+            }
         }
 
-        // Listener para o botão de salvar
+        if (expense == null) {
+            tvTitle.setText(R.string.create_expense_title)
+            btnCreateOrUpdate.setText(R.string.create)
+        } else {
+            tvTitle.setText(R.string.update_expense_title)
+            btnCreateOrUpdate.setText(R.string.update)
+            edtExpenseName.setText(expense.description)
+            edtExpenseAmount.setText(expense.amount.toString())
+            edtExpenseDate.setText(expense.date)
+
+            val currentCategory = categoryList.first { it.name == expense.category }
+            val index = categoryList.indexOf(currentCategory)
+            spinner.setSelection(index)
+        }
+
         btnCreateOrUpdate.setOnClickListener {
             val description = edtExpenseName.text.toString().trim()
-            val amount = edtExpenseAmount.text.toString().toDoubleOrNull() ?: 0.0
-            val date = edtExpenseDate.text.toString()
-            val category = spinner.selectedItem.toString()
-
-            if (description.isNotBlank() && amount > 0 && date.isNotBlank() && category.isNotBlank()) {
-                val expenseData = ExpenseUiData(expense?.id ?: 0, description, amount, category, date)
+            if (expenseCategory != "Select a category" && description.isNotEmpty()) {
                 if (expense == null) {
-                    onCreateClicked.invoke(expenseData)
+                    onCreateClicked.invoke(
+                        ExpenseUiData(
+                            id = 0,
+                            amount = 0.0,
+                            category = requireNotNull(expenseCategory),
+                            date = System.currentTimeMillis().toString(),
+                            description = description
+                        )
+                    )
+                    dismiss()
+                    showMessages("Expense created")
                 } else {
-                    onUpdateClicked.invoke(expenseData)
+                    onUpdateClicked.invoke(
+                        ExpenseUiData(
+                            id = expense.id,
+                            amount = expense.amount,
+                            category = requireNotNull(expenseCategory),
+                            date = expense.date,
+                            description = description
+                        )
+                    )
+                    dismiss()
+                    showMessages("Expense updated")
                 }
-                dismiss()
             } else {
-                showToast("Please fill all fields")
+                showMessages("Fields are required")
             }
         }
 
         return view
     }
 
-    private fun showDatePickerDialog() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        val datePickerDialog = DatePickerDialog(
-            requireContext(),
-            { _, selectedYear, selectedMonth, selectedDay ->
-                val selectedDate = Calendar.getInstance()
-                selectedDate.set(selectedYear, selectedMonth, selectedDay)
-                val dateFormat = SimpleDateFormat("yyyy-MM-dd")
-                val formattedDate = dateFormat.format(selectedDate.time)
-                edtExpenseDate.setText(formattedDate)
-            },
-            year, month, day
-        )
-        datePickerDialog.show()
-    }
     override fun onStart() {
         super.onStart()
 
-
+        // Certifique-se de que o layout da janela seja MATCH_PARENT
         dialog?.window?.setLayout(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.MATCH_PARENT
@@ -123,10 +157,16 @@ class CreateOrUpdateExpenseBottomSheet(
         // Configure o BottomSheetBehavior para estar totalmente expandido
         val bottomSheet = dialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet) as? ViewGroup
         val behavior = bottomSheet?.let { BottomSheetBehavior.from(it) }
-        behavior?.state = BottomSheetBehavior.STATE_EXPANDED
-        behavior?.peekHeight = BottomSheetBehavior.PEEK_HEIGHT_AUTO
+        if (behavior != null) {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+        if (behavior != null) {
+            behavior.peekHeight = BottomSheetBehavior.PEEK_HEIGHT_AUTO
+        }
     }
-    private fun showToast(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+
+
+    private fun showMessages(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
     }
 }
